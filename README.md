0687f70f3e896fc282902bf1591bcca9737480ea
vlouie <v.louie91@gmail.com>
2013-05-31 00:14:53 -0700

    Updated housekeeping files. Comments as guides!

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-tabular-js
+Tabular-JS
 ==========
 
-A simple way to turn your CSV into HTML via JavaScript. Oh my!
+A simple way to turn your CSV into HTML via JS. Oh my!
+
+Included are example files as well as the jQuery library, which Tabular-JS uses to create your table with less mess. As a jQuery plugin, Tabular-JS is easy to use and customize.
--- js/tabular.jQuery.js
+++ js/tabular.jQuery.js
@@ -6,7 +6,18 @@
 
     return this.each( function()}
       $.get(csv, function(data){
+	// TODO: start a new table
+	// store an array of rows
 	var rows = data.split("\n");
+	for (var i = 0; i < rows.length; i++){
+	  // TODO: for each row, add a new row to table
+	  // TODO: account for header?
+	  // store entries of a row as an array
+	  var entry = row[i].split(",");
+	  for (var j = 0; j < entry.length; j++){
+	    // TODO: add new td for each entry
+	  }
+	}
       });
     });
   }
