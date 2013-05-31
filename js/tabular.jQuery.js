df23d3060689023dd0b259de896b8cc7b58a47d9
vlouie <v.louie91@gmail.com>
2013-05-30 22:11:23 -0700

    Fleshing out plugin a bit. TODO: logic!

--- js/tabular.jQuery.js
+++ js/tabular.jQuery.js
@@ -1,5 +1,13 @@
 (function ( $ ) {
-  $.fn.tabular = function( options ){
-    return this;
-  };
+  $.fn.tabular = function( csv, options ){
+    var settings = $.extend({
+
+    }, options);
+
+    return this.each( function()}
+      $.get(csv, function(data){
+	var rows = data.split("\n");
+      });
+    });
+  }
 }( jQuery ));
