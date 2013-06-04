3f721085e64c4cc92ba429e9a4f5f74d858a634c
Victoria Louie <v.louie91@gmail.com>
2013-06-04 10:39:00 -0700

    Basic table generation done!

--- js/tabular.jQuery.js
+++ js/tabular.jQuery.js
@@ -1,5 +1,6 @@
-(function ( $ ) {
+;(function ( $ ) {
   $.fn.tabular = function( csv, options ){
+    var table = this;
     // set default values of options here
     var settings = $.extend({
       tableClass: "",
@@ -10,32 +11,32 @@
       caption: "", // caption tag goes just inside table tag 
       summary: ""
     }, options);
-
-    return this.each( function()}
+    
+    return this.each( function(){
       $.get(csv, function(data){
 	// this = table element
 	// store an array of rows
-	var rows = data.split("\n");
-	for (var i = 0; i < rows.length; i++){
-	  if (i == 0){
-	    // assumes that first row is header
-	    this.append("<thead><tr></tr></thead><tbody></tbody>");
-	  }
-	  else{
-	    $(this).find("tbody").append("<tr></tr>");
-	  }
-	  // store entries of a row as an array
-	  var entry = row[i].split(",");
-	  for (var j = 0; j < entry.length; j++){
-	    if (i == 0){
-	      $(this).find("thead > tr").append("<td>"+entry[j]+"</td>");
-	    }
-	    else{
-	      $(this).find("tbody > tr:last").append("<td>" + entry[j] + "</td>");
-	    }
-	  }
-	}
+        var rows = data.split("\n");
+        for (var i = 0; i < rows.length; i++){
+          if (i == 0){
+            // assumes that first row is header
+            table.append("<thead><tr></tr></thead><tbody></tbody>");
+          }
+          else{
+            table.find("tbody").append("<tr></tr>");
+          }
+          // store entries of a row as an array
+          var entry = rows[i].split(",");
+          for (var j = 0; j < entry.length; j++){
+            if (i == 0){
+              table.find("thead > tr").append("<td>"+entry[j]+"</td>");
+            }
+            else{
+              table.find("tbody > tr:last").append("<td>" + entry[j] + "</td>");
+            }
+          }
+        }
       });
     });
   }
-}( jQuery ));
+})( jQuery );
